fc2fa5fed967cbf8e94e5088d585305551979de3
Bathala-Haresh <hareshbathala87@gmail.com>
2021-09-21 11:42:06 +0530

    Added:program to compare and equal check for feet and inch

---
+++ src/main/java/com/example/Feet.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Feet {
+    private final double value;
+
+    public Feet(double value) {
+        this.value = value;
+    }
+
+    public double getFeetToInch() {
+        return value * 12;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
+}
---
+++ src/main/java/com/example/Inch.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Inch {
+    private final double value;
+
+    public Inch(double value) {
+        this.value = value;
+    }
+
+    public double getInchToFeet() {
+        return value / 12;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Inch inch = (Inch) o;
+        return Double.compare(inch.value, value) == 0;
+    }
+}
